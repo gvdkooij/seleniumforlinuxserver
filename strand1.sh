beb51e4f89ca7cae30affca4dbf9b6e50fa2d6f0
gvdkooij <46501812+gvdkooij@users.noreply.github.com>
2025-03-24 20:14:20 +0100

    Update strand1.sh

--- strand1.sh
+++ strand1.sh
@@ -1,4 +1,5 @@
-sudo apt update && sudo apt full-upgrade -y
+sudo apt update
+sudo DEBIAN_FRONTEND=noninteractive apt full-upgrade -y
 sudo apt install -y python3-pip
 echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
