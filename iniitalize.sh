61748aa1acb9b847c14a150856355a4cfc9b5c8c
gvdkooij <46501812+gvdkooij@users.noreply.github.com>
2025-03-26 10:26:15 +0100

    Update iniitalize.sh

--- iniitalize.sh
+++ iniitalize.sh
@@ -4,6 +4,6 @@ echo "gvdkooij:Rijnweg201" | sudo chpasswd
 sudo usermod -aG sudo gvdkooij
 sudo mkdir -p /home/gvdkooij/scripts
 sudo chown gvdkooij:gvdkooij /home/gvdkooij/scripts
-sudo -u gvdkooij curl -o /home/gvdkooij/scripts/strand1.sh https://raw.githubusercontent.com/gvdkooij/seleniumforlinuxserver/refs/heads/m>
+sudo -u gvdkooij curl -o /home/gvdkooij/scripts/strand1.sh https://raw.githubusercontent.com/gvdkooij/seleniumforlinuxserver/refs/heads/main/strand1.sh
 sudo -u gvdkooij chmod +x /home/gvdkooij/scripts/strand1.sh
 sudo -u gvdkooij /home/gvdkooij/scripts/strand1.sh
